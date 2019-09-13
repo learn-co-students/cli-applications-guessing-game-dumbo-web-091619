eabd75411706b876e81b2e3b22b89a7ccb6c4aaa
Edward A Polanco <eduardpola18@hotmail.com>
2019-09-13 06:14:09 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,26 @@
-# Code your solution here!
+def run_guessing_game
+
+rand 1..6
+my_num = 4
+guess = ""
+guess_count = 0
+guess_limit = 2
+no_more_guesses = true
+
+while guess != my_num
+  if guess_count < guess_limit
+    puts "Enter your guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    no_more_guesses = true
+  end
+end
+
+if guess == my_num
+  puts "You guessed the correct number!"
+elsif guess != my_num
+  puts "sorry the computer guessed" + rand
+else
+  puts Goodbye!
+end 
